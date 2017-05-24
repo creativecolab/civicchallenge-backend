7a2a9f27f3ba80358ffd3090b9b30166aea9197b
Joseph Le <josephle@me.com>
2017-05-23 17:25:59 -0700

    Add challenge seeder and factory

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -22,3 +22,10 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
         'remember_token' => str_random(10),
     ];
 });
+
+$factory->define(App\Challenge::class, function (Faker\Generator $faker) {
+	return [
+		'name' => $faker->sentence(3),
+		'summary' => $faker->paragraph()
+	];
+});
