08c1e67dbd5f4c6f37b6550e84e449aa19450cb6
Joseph Le <josephle@me.com>
2017-05-31 00:22:48 -0700

    Add Category model with updated seeders

---
+++ database/migrations/2017_05_31_070818_create_categories_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->text('description');
+            $table->timestamps();
+        });
+
+        Schema::table('challenges', function (Blueprint $table) {
+        	$table->unsignedInteger('category_id')->nullable()->after('phase');
+        	$table->foreign('category_id')->references('id')->on('categories')->onDelete('set null');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+    	Schema::table('challenges', function (Blueprint $table) {
+    		$table->dropForeign('challenges_category_id_foreign');
+    		$table->dropColumn('category_id');
+	    });
+        Schema::dropIfExists('categories');
+    }
+}
