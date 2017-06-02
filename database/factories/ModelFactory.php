dc6b81f0872d604e72844a05291e6578c0566fa0
Joseph Le <josephle@me.com>
2017-06-02 14:30:24 -0700

    Fix User seeder

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -13,13 +13,11 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 $factory->define(App\User::class, function (Faker\Generator $faker) {
-    static $password;
 
     return [
+	    'slack_id' => 'U' . str_random(6),
         'name' => $faker->name,
-        'email' => $faker->unique()->safeEmail,
-        'password' => $password ?: $password = bcrypt('secret'),
-        'remember_token' => str_random(10),
+        'email' => $faker->unique()->safeEmail
     ];
 });
 
