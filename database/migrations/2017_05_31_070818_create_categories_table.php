53b1dab32b628277d4bf5acb76d5504f6c21a837
Joseph Le <josephle@me.com>
2017-06-09 13:35:44 -0700

    Make category description nullable

--- database/migrations/2017_05_31_070818_create_categories_table.php
+++ database/migrations/2017_05_31_070818_create_categories_table.php
@@ -16,7 +16,7 @@ class CreateCategoriesTable extends Migration
         Schema::create('categories', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->text('description');
+            $table->text('description')->nullable();
             $table->string('thumbnail')->nullable();
             $table->timestamps();
         });
