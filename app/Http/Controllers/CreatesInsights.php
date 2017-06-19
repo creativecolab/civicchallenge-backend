f794fe849a28329d2019166867f0f3f2edd5d830
Joseph Le <josephle@me.com>
2017-06-19 11:09:15 -0700

    Added mapping of ts field to timestamp for Insight controller

--- app/Http/Controllers/CreatesInsights.php
+++ app/Http/Controllers/CreatesInsights.php
@@ -8,12 +8,12 @@ use Illuminate\Http\Request;
 
 trait CreatesInsights {
 	/**
-	 * Creates am Insight object
+	 * Creates an Insight object
 	 *
 	 * @param Request $request
 	 * @param Challenge $challenge
 	 *
-	 * @return Insight
+	 * @return false|\Illuminate\Database\Eloquent\Model
 	 * @internal param $challenge_id
 	 *
 	 */
@@ -28,11 +28,13 @@ trait CreatesInsights {
 			$data['challenge_id'] = $challenge->id;
 		}
 
+		if ( $request->has('ts') ) {
+			$data['timestamp'] = $data['ts'];
+		}
+
 		$insight = new Insight( $data );
 
-		$challenge->insights()->save( $insight );
-
-		return $insight;
+		return $challenge->insights()->save( $insight );
 	}
 
 	/**
@@ -49,15 +51,18 @@ trait CreatesInsights {
 		foreach ( $insights as $key => $insight ) {
 			if ( ! isset( $insight['challenge_id'] ) ) {
 				$insight['challenge_id'] = null;
-				$insight['phase'] = null;
-			}
-			else {
+				$insight['phase']        = null;
+			} else {
 				if ( ! isset( $insight['phase'] ) ) {
-					$insight['phase'] = Challenge::findOrFail($insight['challenge_id'])->phase;
+					$insight['phase'] = Challenge::findOrFail( $insight['challenge_id'] )->phase;
 				}
 			}
 
-			$insights[$key] = $insight;
+			if ( isset( $insight['ts'] ) ) {
+				$insight['timestamp'] = $insight['ts'];
+			}
+
+			$insights[ $key ] = $insight;
 		}
 
 		return Insight::insert( $insights );
