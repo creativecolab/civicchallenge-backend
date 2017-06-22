f3a87089ca1208074366059b5be3824b4603604d
Joseph Le <josephle@me.com>
2017-06-21 19:55:46 -0700

    Add Challenge transformer

--- app/Providers/TransformerServiceProvider.php
+++ app/Providers/TransformerServiceProvider.php
@@ -7,12 +7,13 @@ use Illuminate\Support\ServiceProvider;
 class TransformerServiceProvider extends ServiceProvider {
 	public function register() {
 		$transformers = [
-			'App\Category' => 'App\Transformers\CategoryTransformer',
-			'App\User' => 'App\Transformers\UserTransformer',
+			'App\Category'  => 'App\Transformers\CategoryTransformer',
+			'App\Challenge' => 'App\Transformers\ChallengeTransformer',
+			'App\User'      => 'App\Transformers\UserTransformer',
 		];
 
-		foreach ($transformers as $class => $transformer) {
-			app(\Dingo\Api\Transformer\Factory::class)->register($class, $transformer);
+		foreach ( $transformers as $class => $transformer ) {
+			app( \Dingo\Api\Transformer\Factory::class )->register( $class, $transformer );
 		}
 	}
 }
---
+++ app/Transformers/ChallengeTransformer.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Transformers;
+
+use App\Challenge;
+use League\Fractal\TransformerAbstract;
+
+class ChallengeTransformer extends TransformerAbstract {
+
+	/**
+	 * List of resources possible to include
+	 *
+	 * @var array
+	 */
+	protected $availableIncludes = [
+//		'questions',
+	];
+
+	/**
+	 * Turn this item object into a generic array
+	 *
+	 * @param Challenge $challenge
+	 *
+	 * @return array
+	 */
+	public function transform( Challenge $challenge ) {
+		return [
+			'id'              => (int) $challenge->id,
+			'summary'         => $challenge->summary,
+			'description'     => $challenge->description,
+			'longDescription' => $challenge->long_description,
+			'thumbnail'       => $challenge->thumbnail,
+			'phase'           => $challenge->phase,
+			'createdAt'       => $challenge->created_at->timestamp,
+			'updatedAt'       => $challenge->updated_at->timestamp,
+		];
+	}
+
+}
